e33d45b20dc585c585f4dd9523c523099513e179
JeanCarlos Tavarez <jeancarlos.tavarez1@gmail.com>
2020-04-15 21:25:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
-# code here!
+require "pry"
+class School 
+  attr_accessor :school, :roster 
+  def initialize(school)
+    @school = school 
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    roster[grade] ||= []
+    roster[grade] << student
+  end 
+  
+  def grade(grade)
+    if grade == roster[grade] 
+    roster[student]
+    end
+  end 
+  
+   
+end 
